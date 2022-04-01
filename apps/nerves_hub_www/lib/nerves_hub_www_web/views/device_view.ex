f04a3d7e80e902f053df6bdfbd5e92a483f6cb9f
Bernhard Hackl <bernhard.hackl@sportalliance.com>
2022-04-01 17:16:43 +0200

    Remove unused code

--- apps/nerves_hub_www/lib/nerves_hub_www_web/views/device_view.ex
+++ apps/nerves_hub_www/lib/nerves_hub_www_web/views/device_view.ex
@@ -9,21 +9,9 @@ defmodule NervesHubWWWWeb.DeviceView do
 
   import NervesHubWWWWeb.OrgCertificateView, only: [format_serial: 1]
 
-  def architecture_options do
-    [
-      "aarch64",
-      "arm",
-      "mipsel",
-      "x86",
-      "x86_atom",
-      "x86_64"
-    ]
-  end
-
   def devices_table_header(title, value, current_sort, sort_direction \\ :asc)
 
-  def devices_table_header(title, value, current_sort, sort_direction)
-      when value == current_sort do
+  def devices_table_header(title, value, value = _current_sort, sort_direction) do
     caret_class = if sort_direction == :asc, do: "up", else: "down"
 
     content_tag(:th, phx_click: "sort", phx_value_sort: value, class: "pointer sort-selected") do
@@ -43,20 +31,6 @@ defmodule NervesHubWWWWeb.DeviceView do
 
   def display_status(_), do: nil
 
-  def platform_options do
-    [
-      "bbb",
-      "ev3",
-      "qemu_arm",
-      "rpi",
-      "rpi0",
-      "rpi2",
-      "rpi3",
-      "smartrent_hub",
-      "x86_64"
-    ]
-  end
-
   def tags_to_string(%Ecto.Changeset{} = changeset) do
     changeset
     |> Ecto.Changeset.get_field(:tags)
@@ -66,8 +40,6 @@ defmodule NervesHubWWWWeb.DeviceView do
   def tags_to_string(%{tags: tags}), do: tags_to_string(tags)
   def tags_to_string(tags) when is_list(tags), do: Enum.join(tags, ",")
   def tags_to_string(tags), do: tags
-
-  defdelegate device_status(device), to: Presence
 
   def selected?(filters, field, value), do: filters[field] == value
 
